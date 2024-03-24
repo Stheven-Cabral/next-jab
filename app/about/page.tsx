5dab71ca07f8b5145bc5f429105ee804fe3a8dae
Stheven Cabral <stheven.cabral@gmail.com>
2024-03-24 07:29:54 -0700

    Setup Project

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <main>
+      <Link href="/">Home</Link>
+      <h1>Welcome to the about page</h1>
+    </main>
+  );
+}
